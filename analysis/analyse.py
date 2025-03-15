bd3acd44fd1a4b42f944c8cc9ea928d584f38ea2
Aravind <aravindvchindi@gmail.com>
2025-03-16 02:37:16 +0530

    version 1.1.0 predict_values.py created wrote code to predict the feature values from the test values need to work with comapnay details * some required arrengments made for the analysis.py (removed all the classes and combained them all together into one function

--- analysis/analyse.py
+++ analysis/analyse.py
@@ -6,17 +6,18 @@ import pandas as pd
 import requests
 
 
-class analysis:
+class Company_details:
     def __init__(self, name):
         self.company_name = f'{name}.NS'
         self.yf_api_fetch = yf.Ticker(self.company_name)
         self.company_info = self.yf_api_fetch.info
         self.company_symbol = name
-        self.tradingview_data = self.tradingview_connect(name)
+        self.tv_fetch_data = self.tradingview_connect()
 
-    def tradingview_connect(self, symbol):
+
+    def tradingview_connect(self):
         handler = TA_Handler(
-            symbol=symbol,
+            symbol=self.company_symbol,
             screener="india",
             exchange="NSE",
             interval='1d'
@@ -31,11 +32,16 @@ class analysis:
             "oscillator": oscillator,
             "indicators": indicators
         }
-        return data
-   
+        return data       
+
+
     def share_price(self):
-        return self.company_info['currentPrice']
-    
+        data = self.company_info
+        if 'currentPrice' in data:
+            return data['currentPrice']
+        else:
+            return None
+        
     def share_price_range(self, period='max', interval='1d'):
         stock = self.yf_api_fetch
         share_price_arr = stock.history(period=period, interval=interval)
@@ -52,37 +58,6 @@ class analysis:
             })
         return filtered_data
 
-    # using for company_section in webpage 
-    def company_data(self):
-        row_data_tradingview = self.tradingview_data['indicators']
-        row_data_yfinance = self.yf_api_fetch.info
-        data = {
-            'share_price': row_data_yfinance['currentPrice'],
-            'change':round(row_data_tradingview['change'], 2),
-            'c_name':row_data_yfinance['shortName'],
-            'c_symbol':row_data_yfinance['symbol'].split('.')[0]
-        }
-        return data
-
-class yfinance:
-    def __init__(self, name):
-        self.company_name = f'{name}.NS'
-        self.yf_api_fetch = yf.Ticker(self.company_name)
-        self.company_info = self.yf_api_fetch.info
-        self.company_symbol = name
-
-    def share_price(self):
-        data = self.company_info
-        if 'currentPrice' in data:
-            return data['currentPrice']
-        else:
-            return None
-    # need to work on it detailes saved on notes.
-    def calender(self):
-        data = self.yf_api_fetch
-        calender = data.calendar
-        return calender
-
     def get_nearest_shareprice(self, date):
         date = datetime.strptime(date, '%Y-%m-%d').date()
         start_date = (date - timedelta(days=5)).strftime('%Y-%m-%d')  # start 10 days before the date
@@ -90,43 +65,6 @@ class yfinance:
         shareprice = self.yf_api_fetch.history(start=start_date, end=end_date)
         return round((shareprice['Close'].iloc[0]), 2)
 
-    def company_details(self):
-        company_info = self.company_info
-        def helper(key):
-            if key in company_info:
-                return company_info[key]
-            else:
-                return 1
-        data = {
-            'bussiness': helper('longBusinessSummary'),
-            'share_price': helper('currentPrice'),
-            'change_num': round(helper('currentPrice') - helper('previousClose'), 2),
-            'change_percent': round((100 * (helper('currentPrice')-helper('previousClose'))) / helper('currentPrice'), 2),
-            'c_name': helper('shortName'),
-            'c_symbol': self.company_symbol, 
-            'website': helper('website'),
-            'marketcap':round(helper('marketCap')/10000000),
-            'industry' : helper('industry'),
-            'sector':helper('sector'),
-            'pe' : round(helper('trailingPE'), 2),
-            'pb' : round(helper('priceToBook'), 2),
-            'eps' : helper('trailingEps'),
-            'targetprice' : helper('targetHighPrice'),
-            'fifty2_week_low' : helper('fiftyTwoWeekLow'),
-            'fifty2_week_high' : helper('fiftyTwoWeekHigh'),
-            'divident_yield' : round(helper('dividendYield')*100, 2) if helper('dividendYield') else 0,
-            'bookvalue' : round(helper('bookValue'), 2),
-            'earning_growth' : round(helper('earningsGrowth')*100, 2) if helper('earningsGrowth') else 0,
-            'revenue_growth' : round((helper('revenueGrowth') * 100), 2) if helper('earningsGrowth') else 0,
-            'total_cash' : round(helper('totalCash')/1000000),
-            'total_debt': round(helper('totalDebt')/1000000),
-            'total_revenue':round(helper('totalRevenue')/1000000),
-            'day_low' : helper('dayLow'),
-            'day_high' : helper('dayHigh'),
-            'profit_margin' : round((helper('profitMargins')*100),2),
-        }
-        return data
-    # operating profit margin
     def opm(self, revenue, net_income):
         opm = []
         for rev, ni in zip(revenue, net_income):
@@ -158,25 +96,32 @@ class yfinance:
                 pe.append(0)
         return pe
 
-    def yfinance_data(self):
+    def company_data(self):
         company_info = self.company_info
+        income = self.yf_api_fetch.income_stmt
+        balence = self.yf_api_fetch.balance_sheet
+        cashflow = self.yf_api_fetch.cash_flow
+        years = [date.strftime('%Y') for date in income.columns][::-1]
+        years_length = len(years)
+        tv_indicater_data = self.tv_fetch_data
+        
         def helper(key):
             if key in company_info:
                 return company_info[key]
             else:
                 return 0
         
-        income = self.yf_api_fetch.income_stmt
-        balence = self.yf_api_fetch.balance_sheet
-        cashflow = self.yf_api_fetch.cash_flow
-        years = [date.strftime('%Y') for date in income.columns][::-1]
-        years_length = len(years)
-
         def finance_data_helper(key, df, length):
             if key in df.index:
                 return [data if not pd.isna(data) else 0 for data in df.loc[key]][::-1]
             else:
                 return [1] * length
+        
+        def check(indicator):
+            if indicator in tv_indicater_data['oscillator']['COMPUTE']:
+                return tv_indicater_data['oscillator']['COMPUTE'][indicator]
+            else:
+                return 'NEUTRAL'
 
         # =================== extracting required data from income ==================================
         revenue = finance_data_helper('Total Revenue', income, years_length)
@@ -203,13 +148,25 @@ class yfinance:
         roe = self.roe(net_income, shareholders_equity)
 
         holding = self.holding()
+
         data = {
+            'bussiness': helper('longBusinessSummary'),
+            'share_price': helper('currentPrice'),
+            'change_num': round(helper('currentPrice') - helper('previousClose'), 2),
+            'change_percent': round((100 * (helper('currentPrice')-helper('previousClose'))) / helper('currentPrice'), 2),
+            'c_name': helper('shortName'),
+            'c_symbol': self.company_symbol, 
+            'website': helper('website'),
+            'marketcap':round(helper('marketCap')/10000000),
+            'industry' : helper('industry'),
+            'sector':helper('sector'),
             'dates' : years,
             'revenue' : revenue,
             'operating_expence' : operating_expence,
             'net_income' : net_income,
             'eps' : eps,
             'pe' : round(helper('trailingPE'), 2),
+            'pb' : round(helper('priceToBook'), 2),
             'profit_margin' : profit_margin,
             'total_debt' : total_debt,
             'shareholders_equity' : shareholders_equity,
@@ -221,24 +178,38 @@ class yfinance:
             'operating_cashflow' : operating_cashflow,
             'investing_cashflow' : investing_cashflow,
             'financing_cashflow' : financing_cashflow,
-            'holding' : holding
+            'holding' : holding,
+            'targetprice' : helper('targetHighPrice'),
+            'fifty2_week_low' : helper('fiftyTwoWeekLow'),
+            'fifty2_week_high' : helper('fiftyTwoWeekHigh'),
+            'divident_yield' : round(helper('dividendYield')*100, 2) if helper('dividendYield') else 0,
+            'bookvalue' : round(helper('bookValue'), 2),
+            'earning_growth' : round(helper('earningsGrowth')*100, 2) if helper('earningsGrowth') else 0,
+            'revenue_growth' : round((helper('revenueGrowth') * 100), 2) if helper('earningsGrowth') else 0,
+            'total_cash' : round(helper('totalCash')/1000000),
+            'total_debt': round(helper('totalDebt')/1000000),
+            'total_revenue':round(helper('totalRevenue')/1000000),
+            'day_low' : helper('dayLow'),
+            'day_high' : helper('dayHigh'),
+            'line_data' : {
+                'support1': round(tv_indicater_data['indicators']['Pivot.M.Classic.S1'], 2),
+                'support2': round(tv_indicater_data['indicators']['Pivot.M.Classic.S2'], 2),
+                'support3': round(tv_indicater_data['indicators']['Pivot.M.Classic.S3'], 2),
+                'resistance1': round(tv_indicater_data['indicators']['Pivot.M.Classic.R1'], 2),
+                'resistance2': round(tv_indicater_data['indicators']['Pivot.M.Classic.R2'], 2),
+                'resistance3': round(tv_indicater_data['indicators']['Pivot.M.Classic.R3'], 2)
+            },
+            'indicator_data' : {
+                'summary': tv_indicater_data['oscillator']['RECOMMENDATION'],
+                'rsi': check('RSI'),
+                'adx': check('ADX'),
+                'momentum': check('Mom'),
+                'macd': check('MACD'),
+                'bbp': check('BBP')
+            }
         }
         return data
     
-    # need to work on it where we can use feature excepecially eps_trend
-    def estimation(self):
-        msft = self.yf_api_fetch
-
-        d1 = msft.analyst_price_targets
-        growth_estimation = msft.earnings_estimate
-        d3 = msft.revenue_estimate
-        d4 = msft.earnings_history
-        d5 = msft.eps_trend
-        d7 = msft.growth_estimates
-        d8 = msft.isin
-        d9 = msft.options
-        d10 = msft.news
-        return d10
 
     # Holding patterns for the institution and promotor
     def holding(self):
@@ -250,70 +221,13 @@ class yfinance:
         instituation = round(major_holders.loc['institutionsPercentHeld', 'Value']*100, 2)
         public = round(100 - (insider + instituation), 2)
         return [insider, instituation, public]
-    
-class tradingview:
-    def __init__(self, symbol) -> None:
-        self.c_symbol = symbol
-        self.tv_fetch_data = self.tradingview_connect()
-        
-
-    def tradingview_connect(self):
-        handler = TA_Handler(
-            symbol=self.c_symbol,
-            screener="india",
-            exchange="NSE",
-            interval='1d'
-        )
-        analysis = handler.get_analysis()
-        summary = analysis.summary
-        oscillator  = analysis.oscillators
-        indicators = analysis.indicators
-
-        data = {
-            "summery": summary,
-            "oscillator": oscillator,
-            "indicators": indicators
-        }
-        return data       
-
-    def tradingview_data(self):
-        def check(indicator):
-            if indicator in tv_indicater_data['oscillator']['COMPUTE']:
-                return tv_indicater_data['oscillator']['COMPUTE'][indicator]
-            else:
-                return 'NEUTRAL'
-                
-        yf_connect = yfinance(self.c_symbol)
-        yf_data = yf_connect.company_info
-        tv_indicater_data = self.tv_fetch_data
-        data = {
-            'line_data' : {
-                '52week_high' : yf_data['fiftyTwoWeekHigh'],
-                '52week_low' : yf_data['fiftyTwoWeekLow'],
-                'support1': round(tv_indicater_data['indicators']['Pivot.M.Classic.S1'], 2),
-                'support2': round(tv_indicater_data['indicators']['Pivot.M.Classic.S2'], 2),
-                'support3': round(tv_indicater_data['indicators']['Pivot.M.Classic.S3'], 2),
-                'resistance1': round(tv_indicater_data['indicators']['Pivot.M.Classic.R1'], 2),
-                'resistance2': round(tv_indicater_data['indicators']['Pivot.M.Classic.R2'], 2),
-                'resistance3': round(tv_indicater_data['indicators']['Pivot.M.Classic.R3'], 2)
-            },
-            'indicator_data' : {
-            'summary': tv_indicater_data['oscillator']['RECOMMENDATION'],
-            'rsi': check('RSI'),
-            'adx': check('ADX'),
-            'momentum': check('Mom'),
-            'macd': check('MACD'),
-            'bbp': check('BBP')
-            }
-        }
-        return data
 
 
 if __name__ == "__main__":
     # Example usage
-    company_symbol = 'HDFCBANK'                     #RELIANCE
-    data = tradingview(company_symbol)   
-    tv_data = data.tradingview_data()
+    company_symbol = 'RELIANCE'
+    data = yfinance(company_symbol)   
+    tv_data = data.yfinance_data()
     print(tv_data)
     # print("=================== income ========================")
     # print(income_stmt['dates'])
--- project.py
+++ project.py
@@ -1,9 +1,23 @@
-import pandas as pd
-from analysis import analysis
+from analysis import Company_details
+
+def share_price_arr(c_name):
+    yfinance_data = Company_details(c_name)
+    share_price_arr = yfinance_data.share_price_range('1mo', '1d')
+    return share_price_arr
+
+def company_details(c_name):
+    yfinance_data = Company_details(c_name)
+    company_info = yfinance_data.company_data()
+    return company_info
+
+        
 
 if __name__ == '__main__':
-    c_name = 'ITC'
-    details = analysis(c_name)
+    c_names = ['RELIANCE','HDFCBANK', 'TCS', 'KOTAKBANK', 'BAJFINANCE', 'BAJAJFINSV', 'SBILIFE']
 
-    df = pd.DataFrame(details.share_price_range('1mo', '1d'))
-    print(df)
+    for c_name in c_names:
+        details = company_details(c_name)
+        print(f'========================== {c_name} ==============================')
+        for data in details:
+            print(f'{data} : {details[data]}')
+        print('===================================================================')
