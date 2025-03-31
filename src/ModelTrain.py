665835259d8679df19cc041f24ecb8bacddff05b
Aravind <aravindvchindi@gmail.com>
2025-03-31 23:04:09 +0530

    version 2.2.0 to train the model and predict the model .py files created [ sharePricePrediction.py, modelTrain.py ] modelTrain.py able train the model based on user company details sharePricePrediction.py file is able to predict the future values by mentioning the future_years

---
+++ src/ModelTrain.py
@@ -0,0 +1,128 @@
+import datetime
+from APICall import CompanyDetails
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from keras.models import Sequential, load_model
+from keras.layers import LSTM, Dense, Input, Dropout
+from keras.regularizers import l2
+kernel_regularizer=l2(0.01)
+from keras.optimizers import Adam
+
+class ModelTrain:
+    def __init__(self, company_list, model_name, window_size=5, price_range='25y'):
+        self.c_list = company_list
+        self.window_size = window_size
+        self.price_range = price_range
+        self.companies_df = self.loadCompany()
+        self.x_all, self.y_all = self.loadAndCombineData()
+        self.model = self.trainModel(model_name)
+    
+    def loadCompany(self):
+        companies_df = []
+        for company in self.c_list:
+            company_details = CompanyDetails(company)
+            share_price_df = company_details.sharePriceRange(self.price_range)
+            share_price_df['Date'] = share_price_df['Date'].apply(lambda d: datetime.datetime.strptime(d, '%Y-%m-%d')).sort_index()
+            share_price_df.index = share_price_df.pop('Date')
+            companies_df.append(share_price_df)
+            print(f'{company} added to Companies_df')
+        
+        print("All companies share-price data added to the companies df list")
+        return companies_df
+
+    def createWindowsForCompanies(self, company_df):
+        data_x, data_y = [], []
+
+        company_df = company_df.sort_index()
+        close_vals = company_df['Close'].values
+        ema_vals = company_df['ema200'].values
+
+        for i in range(len(company_df) - self.window_size):
+            x_window = []
+            for window in range(self.window_size):
+                x_window.append([close_vals[i + window], ema_vals[i + window]])
+            
+            target = close_vals[i + self.window_size]
+            data_x.append(x_window)
+            data_y.append(target)
+        
+        print('Creating the window for the companies done.')
+
+        return np.array(data_x), np.array(data_y)
+    
+    def loadAndCombineData(self):
+        x_combined, y_combined = [], []
+
+        for df in self.companies_df:
+            x, y = self.createWindowsForCompanies(df)
+            if len(x) > 0:
+                x_combined.append(x)
+                y_combined.append(y)
+        
+        if len(x_combined) == 0:
+            raise ValueError("No data found after window creation.")
+
+        x_all = np.concatenate(x_combined, axis=0)
+        y_all = np.concatenate(y_combined, axis=0)
+
+        print('All the companies share price combained into x_all and y_all')
+
+        return x_all, y_all
+    
+    def trainModel(self, model_name):
+        num_samples = len(self.x_all)
+
+        train_end = int(num_samples * 0.8)
+        valid_end   = int(num_samples * 0.9)
+
+        x_train, y_train = self.x_all[:train_end], self.y_all[:train_end]
+        x_valid, y_valid   = self.x_all[train_end:valid_end], self.y_all[train_end:valid_end]
+        x_test, y_test  = self.x_all[valid_end:], self.y_all[valid_end:]
+
+        model = Sequential()
+
+        model.add(LSTM(units=120, activation='relu', return_sequences=True, input_shape=(self.x_all.shape[1], self.x_all.shape[2]), kernel_regularizer=l2(0.01), recurrent_dropout=0.2))
+
+        model.add(LSTM(units=64, activation='relu', return_sequences=True, kernel_regularizer=l2(0.01), recurrent_dropout=0.2))
+
+        model.add(LSTM(units=32, activation='relu', return_sequences=False, kernel_regularizer=l2(0.01), recurrent_dropout=0.2))
+
+        model.add(Dense(32, activation='relu', kernel_regularizer=l2(0.01)))
+        model.add(Dropout(0.2))
+
+        model.add(Dense(16, activation='relu', kernel_regularizer=l2(0.01)))
+        model.add(Dropout(0.2))
+
+        # final output layer
+        model.add(Dense(1, activation='linear'))
+
+        model.compile(
+            loss='mse',
+            optimizer=Adam(learning_rate=0.001),
+            metrics=['mae']
+        )
+
+        model.fit(
+            x_train, y_train,
+            validation_data=(x_valid, y_valid),
+            epochs=10,
+            batch_size=32
+        )
+
+        test_loss, test_mae = model.evaluate(x_test, y_test)
+        print(f"Test MSE: {test_loss:.4f}, Test MAE: {test_mae:.4f}")
+        
+        # below command save the model to the current dir
+        model.save(f'{model_name}.keras')
+
+        print(f'{model_name} saved to current dir')
+        return model
+    
+    def getModel(self):
+        return self.model
+    
+if __name__ == '__main__':
+    c_list = ['ITC', 'ONGC']
+    model = ModelTrain(company_list=c_list, model_name='Aravind_test', price_range='2y')
+    print({'get model':model.getModel()})
--- src/sharePricePrediction.py
+++ src/sharePricePrediction.py
@@ -1,192 +1,116 @@
-from src import CompanyDetails
-from keras.models import Sequential
+import datetime
+from .APICall import CompanyDetails
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from keras.models import Sequential, load_model
 from keras.layers import LSTM, Dense, Input
 from keras.optimizers import Adam
-from keras.callbacks import EarlyStopping
-import numpy as np
-import pandas as pd
-import datetime
 
 class SharePricePrediction:
-    def __init__(self, c_name):
-        self.c_name = c_name
-        self.company_details = CompanyDetails(c_name)
+    def __init__(self, company_name, model_name, price_range=5, window_size=5):
+        self.c_name = company_name
+        self.model_name = model_name
+        self.price_range = price_range
+        self.window_size = window_size
+        self.company_details = CompanyDetails(self.c_name)
+        self.share_price_df = self.sharePriceDF()
+        self.model = self.findModel()
+    
+    def findModel(self):
+        try:
+            self.model = load_model(f'{self.model_name}.keras')
+            return self.model
+        except (OSError, ValueError) as e:
+            print(f"Error loading model '{self.model_name}': {e}")
+            raise RuntimeError("Model not found, exiting class.") from e
 
-    def str_to_datetime(self, date_str):
-        return datetime.datetime.strptime(date_str, '%Y-%m-%d')
-
-
-    def prediction(self, first_date_str, last_date_str, pred_range='2y', pred_intvel='1d'):
-        # last_date = datetime.datetime.now().date()
-        # first_date = last_date - datetime.timedelta(days=365 * 2)
-        # first_date_str = str(first_date)
-        # last_date_str = str(last_date)
-        
-        share_price_df = self.company_details.sharePriceRange(pred_range, pred_intvel)
-        share_price_df['Date'] = share_price_df['Date'].apply(lambda d: datetime.datetime.strptime(d, '%Y-%m-%d'))
+    def sharePriceDF(self):
+        share_price_df = self.company_details.sharePriceRange(f'{self.price_range}y')
+        share_price_df['Date'] =share_price_df['Date'].apply(lambda d: datetime.datetime.strptime(d, '%Y-%m-%d')).sort_index()
         share_price_df.index = share_price_df.pop('Date')
-        windowed_df = self.df_to_windowed_df(share_price_df, first_date_str, last_date_str)
-        
-        if windowed_df is None:
-            raise ValueError("Windowed dataframe creation failed due to insufficient data.")
-        
-        train_test_validation = self.trainTestValidation(windowed_df)
+        return share_price_df.sort_index()
+    
+    def createWindowForInstance(self):
+        data_x = []
+        close_vals = self.share_price_df['Close'].values
+        ema_vals = self.share_price_df['ema200'].values
+
+        for i in range(len(self.share_price_df) - self.window_size):
+            windows = []
+            for window in range(self.window_size):
+                windows.append([close_vals[i + window], ema_vals[i + window]])
+            data_x.append(windows)
+
+        return np.array(data_x)
+    
+    def sharePricePrediction(self):
+        window_for_instance = self.createWindowForInstance()
+        price_prediction = self.model.predict(window_for_instance)
+        prediction_dates = self.share_price_df.index[self.window_size:]
+        result_df = pd.DataFrame()
+        result_df['Date'] = prediction_dates
+        result_df['Close'] = price_prediction.flatten()
+        result_df['ema200'] = price_prediction.flatten()
+        result_df.set_index('Date', inplace=True)
+        return result_df
+    
+    def futurePricePrediction(self, future_years=5):
+        last_row = self.share_price_df.iloc[-1]
+        know_date = self.share_price_df.index[-1]
+        last_close = last_row['Close']
+        last_ema = last_row['ema200']
+        last_window = self.share_price_df[['Close', 'ema200']].tail(self.window_size).to_numpy()
+
+        future_months = []
+        current_date = self.share_price_df.index[-1]
+
+        # crating the future month
+        for _ in range(future_years * 12):
+            year, month = current_date.year, current_date.month
+            month += 1
+            if month > 12:
+                month = 1
+                year += 1
+            current_date = datetime.datetime(year=year, month=month, day=1)
+            future_months.append(current_date)
+
+        # finding future shareprices
+        alpha = 2 / 201.0
+        future_close = []
+        future_ema = []
+
+        current_window = last_window.copy()
+        for _ in range(future_years * 12):
+            input_data = np.expand_dims(current_window, axis=0)
+            pred_close = self.model.predict(input_data)
+            pred_close_val = pred_close.flatten()[0]
+            last_ema = current_window[-1, 1]
+            new_ema = (pred_close_val - last_ema) * alpha + last_ema
+
+            future_close.append(pred_close_val)
+            future_ema.append(new_ema)
+
+            new_row = np.array([[pred_close_val, new_ema]])
+            current_window = np.vstack([current_window[1:], new_row])
+
+        future_price_df = pd.DataFrame()
+        future_price_df['Date'] = future_months
+        future_price_df['Close'] = future_close
+        future_price_df['ema200'] = future_ema
+
+        return future_price_df
+    
+    def future_share_price(self, future_years=5):
+        previous_share_price = self.sharePricePrediction()
+        future_share_price = self.futurePricePrediction(future_years=future_years)
         return {
-                'train_test_validation': train_test_validation, 
-                'share_price_df' : share_price_df
-            }
-
-
-    def extractWindow(self, dataframe, target_date, n):
-        window = dataframe.loc[:target_date].tail(n+1)
-        if len(window) != n+1:
-            raise ValueError(f'Window of size {n} is too large for date {target_date}')
-        values = window['Close'].to_numpy()
-        x = values[:-1]
-        y = values[-1]
-        return x, y
-    
-    def getNextTargetDate(self, dataframe, current_date, days_ahead=7):
-        # Ensure the dates are sorted
-        dates = dataframe.index.sort_values()
-        
-        if current_date not in dates:
-            pos = dates.searchsorted(current_date)
-            if pos < len(dates):
-                current_date = dates[pos]
-            else:
-                return None  # No further dates available.
-        
-        pos = dates.get_loc(current_date)
-        # If current_date is the last available date, return None.
-        if pos >= len(dates) - 1:
-            return None
-        
-        for next_date in dates[pos+1:]:
-            if next_date <= current_date + datetime.timedelta(days=days_ahead):
-                return next_date
-            else:
-                break  # Exit if the date is beyond the window.
-        
-        # Fallback: return the immediate next available date.
-        return dates[pos+1]
-    
-    def buildWindowedDF(self, dates, X, Y, n):
-        ret_df = pd.DataFrame()
-        ret_df['Target Date'] = dates
-        X = np.array(X)
-        for i in range(n):
-            ret_df[f'Target-{n-i}'] = X[:, i]
-        ret_df['Target'] = Y
-        return ret_df
-    
-    def df_to_windowed_df(self, dataframe, first_date_str, last_date_str, n=3):
-        first_date = self.str_to_datetime(first_date_str)
-        last_date = self.str_to_datetime(last_date_str)
-
-        target_date = first_date
-        dates = []
-        X, Y = [], []
-
-        while True:
-            try:
-                x, y = self.extractWindow(dataframe, target_date, n)
-            except ValueError as e:
-                print(e)
-                return None
-
-            dates.append(target_date)
-            X.append(x)
-            Y.append(y)
-
-            # Get the next available target date.
-            next_date = self.getNextTargetDate(dataframe, target_date, days_ahead=7)
-
-            # Exit if no further date is available or the next date is beyond the last date.
-            if next_date is None or next_date > last_date:
-                break
-
-            target_date = next_date
-
-        return self.buildWindowedDF(dates, X, Y, n)
-    
-    def windowTodateXY(self, window_df):
-        # Converts every row of df to an array 
-        df_as_np = window_df.to_numpy()
-        # df_as_np o/p = [[Timestamp('2023-03-21 00:00:00') 361.41 355.82 358.9 357.15]..........]
-
-        # first colume of window_df is about dates
-        # : = hole df_as_np array's [0]'th val means dates colume
-        # o/p = [Timestamp('2023-03-21 00:00:00')]
-        dates = df_as_np[:, 0]
-
-        # it is about window_df[share_price_values]
-        # : = hole df_as_np array's [1st_colume] to [last_colume]
-        # o/p = [361.41 355.82 358.9] accept target value
-        middle_matrix = df_as_np[:, 1:-1]
-
-        '''
-            o/p = [[[361.41]
-                    [355.82]
-                    [358.9]]
-                    .........
-                    .........]]    
-        '''
-        data_x = middle_matrix.reshape((len(dates), middle_matrix.shape[1], 1))
-        
-        # below line gives output with only df_values
-        # : means hole [0] end of err    -1 = second part of values
-        # o/p = [357.15 357.91 360.65 358.81 360.23]
-        data_y = df_as_np[:, -1]
-        return dates, data_x.astype(np.float32), data_y.astype(np.float32)
-    
-    def trainTestValidation(self, windowed_df):
-        dates, data_x, data_y = self.windowTodateXY(windowed_df)
-
-        q_80 = int(len(dates) * .8)
-        q_90 = int(len(dates) * .9)
-
-        # Training dataset
-        dates_train, x_train, y_train = dates[:q_80], data_x[:q_80], data_y[:q_80]
-
-        # Validation dataset
-        dates_valid, x_valid, y_valid = dates[q_80:q_90], data_x[q_80:q_90], data_y[q_80:q_90]
-
-        # testing dataset
-        dates_test, x_test, y_test = dates[q_90:], data_x[q_90:], data_y[q_90:]
-
-        model = self.model_train(x_train, y_train, x_valid, y_valid)
-
-        train_prediction = model.predict(x_train).flatten()
-
-        valid_prediction = model.predict(x_valid).flatten()
-
-        test_prediction = model.predict(x_test).flatten()
-
-        return {
-            'dates_train' : dates_train,
-            'y_train' : y_train,
-            'train_prediction' : train_prediction,
-            'dates_valid' : dates_valid,
-            'y_valid' : y_valid,
-            'valid_prediction' : valid_prediction,
-            'dates_test' : dates_test,
-            'y_test' : y_test,
-            'test_prediction' : test_prediction
+            'previous_share_price' : previous_share_price,
+            'future_share_price' : future_share_price
         }
 
-    def model_train(self, x_train, y_train, x_valid, y_valid):
-        model = Sequential([Input((3, 1)),
-                            LSTM(64),
-                            Dense(32,activation='relu'),
-                            Dense(32, activation='relu'),
-                            Dense(1)])
-        
-        model.compile(loss='mse',
-                      optimizer=Adam(learning_rate=0.001),
-                      metrics=['mean_absolute_error'])
-        
-        early_stop = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
-        model.fit(x_train, y_train, validation_data=(x_valid, y_valid), epochs=100, callbacks=[early_stop])
-
-        return model
+    
+if __name__ == '__main__':
+    c_name = 'ITC'
+    future = SharePricePrediction(company_name=c_name, model_name='Aravind_test')
